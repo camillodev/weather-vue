ddc40dd37be21cd1d2fa84abb6cdb9c2c62ee82d
Rafael Camillo <rafael.camillo@iteris.com.br>
2020-12-03 20:56:07 -0300

    stating project

---
+++ src/components/SearchLocation.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search-location">
+    <b-form @submit="searchLocation">    
+      <b-input-group size="lg">
+        <b-form-input id="search" v-model="form.search" type="search" placeholder="Bruxellas"></b-form-input>
+        <b-button variant="light" size="lg" @click="searchLocation"> <b-icon icon="search" aria-hidden="true"></b-icon></b-button>
+      </b-input-group>
+    </b-form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .search-location {
+    margin: 0 auto;
+    align-self: center;
+    width: 70%;
+
+    input {
+      border-radius: 10px 0 0 10px;
+    }
+    button {
+      border-radius: 0 10px 10px 0;
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: "SearchLocation",
+  components: { },
+  data() { 
+    return {
+      form: { 
+        search: ""
+      }
+    };
+  },
+  props: {},
+  computed: {}, 
+  methods: {
+      searchLocation(evt) {
+          evt.preventDefault()
+          alert(this.form.search)
+      }
+
+  }
+
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div class="container d-flex">
+        <SearchLocation />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .home {
+    display: flex;
+    height: 100vh;
+    background: url("../assets/cloudly-rj.jpg") no-repeat center center fixed; 
+    background-size: cover;
+  }
+</style>
+
+<script>
+// @ is an alias to /src
+import SearchLocation from "@/components/SearchLocation.vue";
+
+export default {
+  name: "Home",
+  components: {
+    SearchLocation
+  }
+};
+</script>
